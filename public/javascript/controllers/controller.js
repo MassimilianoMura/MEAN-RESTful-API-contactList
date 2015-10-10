30eae6111b1afdd52c7e4dccb552bc538aa3c72a
MassimilianoMura <massimilianomura1983@gmail.com>
2015-10-10 16:10:25 +0100

    moves dummy data in the server and uses a GET request

--- public/javascript/controllers/controller.js
+++ public/javascript/controllers/controller.js
@@ -1,20 +1,9 @@
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
-  person1 = {
-    name: 'Massi',
-    email: 'massi@email.com',
-    number: '111-111111'
-  };
-
-  person2 = {
-    name: 'Ted',
-    email: 'ted@email.com',
-    number: '222-222222'
-  };
-
-  var contactList = [person1, person2];
-
-  $scope.list = contactList;
+$http.get('/contactlist').success(function (response) {
+  console.log('I\'ve got the response to the GET request')
+  $scope.list = response;
+})
 
 
 
--- server.js
+++ server.js
@@ -3,5 +3,25 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/contactlist', function (req, res) {
+  console.log('I received a GET request ')
+
+   person1 = {
+    name: 'Massi',
+    email: 'massi@email.com',
+    number: '111-111111'
+  };
+
+  person2 = {
+    name: 'Ted',
+    email: 'ted@email.com',
+    number: '222-222222'
+  };
+
+  var contactList = [person1, person2];
+
+  res.json(contactList)
+})
+
 app.listen(3000);
 console.log('server running at port 3000');
