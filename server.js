64030f24d901eea3e7409b4ed66f2a40a5ac2882
MassimilianoMura <massimilianomura1983@gmail.com>
2015-10-10 16:59:29 +0100

    takes off dummy data and starts using mondoDB

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db =mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/contactlist', function (req, res) {
   console.log('I received a GET request ')
 
-   person1 = {
-    name: 'Massi',
-    email: 'massi@email.com',
-    number: '111-111111'
-  };
-
-  person2 = {
-    name: 'Ted',
-    email: 'ted@email.com',
-    number: '222-222222'
-  };
-
-  var contactList = [person1, person2];
-
-  res.json(contactList)
-})
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  })
+});
 
 app.listen(3000);
 console.log('server running at port 3000');
