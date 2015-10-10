a7eade5723f91e080571aef233438a46edaeaac1
MassimilianoMura <massimilianomura1983@gmail.com>
2015-10-10 15:30:32 +0100

    displays list using dummy data

--- public/javascript/controllers/controller.js
+++ public/javascript/controllers/controller.js
@@ -1,3 +1,21 @@
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
+  person1 = {
+    name: 'Massi',
+    email: 'massi@email.com',
+    number: '111-111111'
+  };
+
+  person2 = {
+    name: 'Ted',
+    email: 'ted@email.com',
+    number: '222-222222'
+  };
+
+  var contactList = [person1, person2];
+
+  $scope.list = contactList;
+
+
+
 }]);
